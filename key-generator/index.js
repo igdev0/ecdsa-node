47f8682ba0ae26e8c5de7449de9de4aafb64b103
igdev <dorultanianos@gmail.com>
2025-03-27 22:24:11 +0200

    chore: modify output format

--- key-generator/index.js
+++ key-generator/index.js
@@ -5,5 +5,5 @@ const secp = require("ethereum-cryptography/secp256k1");
 const privateKey = secp.secp256k1.utils.randomPrivateKey();
 const publicKey = secp.secp256k1.getPublicKey(privateKey);
 
-console.log(`\n\n🔑 Private Key: ${utils.toHex(privateKey)}`);
-console.log(`🔒 Public Key: ${utils.toHex(publicKey)}\n\n`);
+console.log(`\n\n🔒 Public Key: 0x${utils.toHex(publicKey)}`);
+console.log(`🔑 Private Key: ${utils.toHex(privateKey)}\n\n`);
